eeb91cca7d61e75765ab3ce718dbaf49262c6999
Ryan Allen <ryanallen3413@icloud.com>
2026-03-01 17:11:03 -0600

    fixed xml formatting

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 public class Main {
-
     public static void main(String[] args) {
         Movie regularMovie = new Movie("The Godfather", Movie.REGULAR);
         Movie newReleaseMovie = new Movie("Independence Day", Movie.NEW_RELEASE);
@@ -10,8 +9,6 @@ public class Main {
         customer.addRental(new Rental(newReleaseMovie, 2));
         customer.addRental(new Rental(childrensMovie, 4));
 
-        System.out.println(customer.statement());
-        System.out.println();
         System.out.println(customer.statementXml());
     }
 }
